108ea6246b94a2b01dba091b8ebb316fb29b29ae
Miki247 <130936744+Miki247@users.noreply.github.com>
2023-05-01 15:39:15 +0200

    Added Movement.h with related main functions

---
+++ Movement.h
@@ -0,0 +1,76 @@
+#include <cstdint>
+#include "conio.h"
+
+typedef struct {
+    float x;
+    float y;
+    float xSpeed;
+    float ySpeed;
+    bool isMushroom;
+    bool hasJumped;
+} Mario;
+
+#define MAX_MOVE_SPEED  7
+
+void moveMario(Mario *mario, float timeDiff){
+    if(mario->xSpeed != 0){
+        mario->x += timeDiff * mario->xSpeed;
+        if (mario->x < -7){
+            mario->x = -7;
+        }
+    }
+    if(mario->y <= 0){              // Replace with ground collision later
+        mario->y = 0;
+        mario->ySpeed = 0;
+        mario->hasJumped = false;
+    }
+    if(mario->ySpeed != 0){
+        mario->y += timeDiff * mario->ySpeed;
+        mario->ySpeed -= 0.1;
+    }
+}
+
+void reduceSpeed(Mario *mario){
+    if (mario->xSpeed > 0){
+        mario->xSpeed -= 0.5;
+        if(mario->xSpeed < 0){
+            mario->xSpeed = 0;
+        }
+    } else if (mario->xSpeed < 0){
+        mario->xSpeed += 0.5;
+        if (mario->xSpeed > 0){
+            mario->xSpeed = 0;
+        }
+    }
+}
+
+void checkKey(Mario *mario){
+    char in;
+    if(kbhit()){
+        in = getch();
+        if (in == 'a'){
+            /*
+            if(mario->xSpeed > -MAX_MOVE_SPEED){
+                mario->xSpeed -= 0.1;
+            }
+             */
+            mario->xSpeed = -MAX_MOVE_SPEED;
+        }else if (in == 'd'){
+            /*
+            if(mario->xSpeed < MAX_MOVE_SPEED){
+                mario->xSpeed += 0.1;
+            }
+             */
+            mario->xSpeed = MAX_MOVE_SPEED;
+        }
+
+        if (in == 'w' && !mario->hasJumped){
+            mario->ySpeed = 11;
+            mario->hasJumped = true;
+            mario->y = 0.00001;
+        }
+    } /*else if (!kbhit() && !mario->hasJumped){
+        reduceSpeed(mario);
+    }
+    */
+}
--- main.cpp
+++ main.cpp
@@ -1,7 +1,26 @@
+#include <stdio.h>
 #include <iostream>
-#include "MapLoader.h"
+#include "conio.h"
+#include "Movement.h"
+#include "time.h"
 
-int main() {
-    std::cout << "Hello, World!" << std::endl;
+int main(){
+    struct timespec start, end;
+    clock_gettime(CLOCK_MONOTONIC, &start);
+    Mario mario = {0, 0, 0, 0, false, false};
+    float oldLocationX = 0, oldLocationY = 0;
+    clock_gettime(CLOCK_MONOTONIC, &end);
+    while (true){
+        checkKey(&mario);
+        moveMario(&mario, 0.01);
+        if (oldLocationX != mario.x || oldLocationY != mario.y){            //Replace with check if something updated
+            system("cls");
+            printf("x: %f \t y: %f", mario.x, mario.y);
+            oldLocationX = mario.x;
+            oldLocationY = mario.y;
+        }
+
+        mario.xSpeed = 0;
+    }
     return 0;
 }
