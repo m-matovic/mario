e121872c8bf4ef52c87b1c54938accb0d1f9d942
MarkoM75 <marko.p.matovic@gmail.com>
2023-05-11 13:33:41 +0000

    Separate start menu, and start working on drawing a real background

--- ui.h
+++ ui.h
@@ -4,8 +4,9 @@
 static void error_callback(int e, const char *d);
 void glfwinit(const char *wintag);
 int shouldEnd(void);
+int menu(void);
 void frminit(void);
-void frmadd(void);
+void drawbg(struct nk_image bg);
 void frmdraw(void);
 void glfwend(void);
 
