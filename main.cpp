817511571f77b28abeea3c969dc6b7bacc7dca51
mihajlo <urosevicmihajlo@gmail.com>
2023-05-27 12:24:09 +0200

    Made mushrooms spawn when hitting blocks and added so lava kills the player

--- main.cpp
+++ main.cpp
@@ -30,7 +30,7 @@ int main(void)
         frame_draw();
     }
     
-    int world = 4;
+    int world = 1;
     MapViewport *map = mapInit("worlds/" + std::to_string(world));
 
     load_backgrounds();
@@ -41,7 +41,7 @@ int main(void)
     gettimeofday(&current, NULL);
     double currentTime = current.tv_sec %10 + (double) current.tv_usec / 1000000;
 
-    EntityNode *mario = summonEntity(MARIO, 120, 5, map->map);
+    EntityNode *mario = summonEntity(MARIO, 2, 5, map->map);
     mario->velX = 0;
     double shifter = 0;
     float direction = -EPS;
